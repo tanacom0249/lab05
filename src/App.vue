1080012bf084453b01db2235fcdcecf02138c1d0
tanacom0249 <tanacom0249@hotmail.co.th>
2023-01-17 00:15:11 +0700

    Frome input Data  นายธนาคม 6404101363

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup >
+import {ref} from "vue"
+const stuID =ref("");
+const stuName = ref("");
+</script>
+
+<template>
+  <h1>หน้าหลัก.  แบบฟอร์มลาออก</h1>
+  <form>
+  วันที่ <input type="date" name="" id="">
+  คณะ
+  <select name="" id="">
+    <option>===กรุณาเลือกคณะ===</option>
+    <option>คณะวิทยาศาสตร์</option>
+    <option>คณะบริหาร</option>
+    <option>คณะท่องเที่ยว</option>
+    <option>คณะศิลปศาสตร์</option>
+    <option>คณะสารสนเทศ</option>
+    <option>คณะผลิตกรรมการเกษตร</option>
+    <option>คณะพยาบาล</option>
+    <option>คณะพืชไร่</option>
+  </select>
+  สาขา
+  <select subject="">
+    <option>===กรุณาเลือกสาขา===</option>
+    <option>สาขาวิทยาศาสตร์</option>
+    <option>สาขาบริหารธุรกิจ</option>
+    <option>สาขาเทคโนโลยีการท่องเที่ยว</option>
+    <option>สาขาศิลปะ</option>
+    <option>สาขาเทคโนโลยีสารสนเทศ</option>
+    <option>สาขาเทคโนโลยีการเกษตร</option>
+    <option>คณะพยาบาล</option>
+    <option>สาขาพืชไร่</option>
+  </select>
+    รหัสนศ. <input type="text" name="" id="" required>
+    ชื่อ <input type="text" name="" id="" maxlength="100" required>
+    หมายเลขโทรศัพท์ <input type="tel" neme="" id="" maxlength="10">
+    <input type="submit" value="ยืนยันลาออก" />
+  </form>
+ {{ stuID }} {{ stuName }}
+</template>
+
+<style scoped></style>
---
+++ src/components/LikeSad.vue
@@ -0,0 +1,23 @@
+<script setup >
+import { ref } from "vue";
+const like = ref(50);
+const sed = ref(100);
+
+function  likeClick() {
+  like.value +=50;
+}
+function sadClick(){
+  sed.value -=500;
+}
+
+</script>
+
+<template>
+ <div>
+   <button @click="likeClick()">ปุ่ม Like</button>
+   {{ like }}
+   <button @click="sedClick()">ปุ่ม Sad</button> {{ sad }}
+   </div>
+</template>
+
+<style scoped></style>
